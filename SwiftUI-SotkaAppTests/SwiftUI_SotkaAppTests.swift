f0e6cebcb8113ff9d6dc4c85f57d9726e51adcf0
Oleg <51740599+easydev991@users.noreply.github.com>
2025-05-04 11:11:41 +0300

    Initial Commit

---
+++ SwiftUI-SotkaAppTests/SwiftUI_SotkaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SotkaAppTests.swift
+//  SwiftUI-SotkaAppTests
+//
+//  Created by Олег Еременко on 04.05.2025.
+//
+
+import Testing
+@testable import SwiftUI_SotkaApp
+
+struct SwiftUI_SotkaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
