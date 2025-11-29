3edd675dc325d2bfc54133147909e7395dcf180a
Oleg <51740599+easydev991@users.noreply.github.com>
2025-11-29 23:22:42 +0300

    Поправил баг с декодированием ответа сервера

--- SwiftUI-SotkaApp/Models/CurrentRunResponse.swift
+++ SwiftUI-SotkaApp/Models/CurrentRunResponse.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SWUtils
 
 /// Текущее прохождение программы
 struct CurrentRunResponse: Decodable {
@@ -9,4 +10,20 @@ struct CurrentRunResponse: Decodable {
 
     /// Максимальный день, до которого доступны инфопосты
     let maxForAllRunsDay: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case date
+        case maxForAllRunsDay
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.date = container.decodeISO8601DateIfPresent(.date)
+        self.maxForAllRunsDay = container.decodeIntOrNilIfPresent(.maxForAllRunsDay)
+    }
+
+    init(date: Date? = nil, maxForAllRunsDay: Int? = nil) {
+        self.date = date
+        self.maxForAllRunsDay = maxForAllRunsDay
+    }
 }
